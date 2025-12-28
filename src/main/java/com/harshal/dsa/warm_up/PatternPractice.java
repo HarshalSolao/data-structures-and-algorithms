90d548aff9619d09405bf02248c4e3d3c78f9570
Harshal Solao <harshalsolao@gmail.com>
2025-12-28 15:40:10 +0530

    Pattern added with class modification

---
+++ src/main/java/com/harshal/dsa/warm_up/PatternPractice.java
@@ -0,0 +1,31 @@
+package com.harshal.dsa.warm_up;
+
+public class PatternPractice {
+    public static void main(String[] args) {
+        printInvertedStarTriangle();
+        System.out.println("----------------------");
+        printDescendingNumberTriangle();
+    }
+
+    public static void printInvertedStarTriangle(){
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            String row = "";
+            for (int j = 0; j < (n - i); j++) {
+                row = row + "* ";
+            }
+            System.out.println(row);
+        }
+    }
+
+    public static void printDescendingNumberTriangle(){
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            String row = "";
+            for (int j = 0; j < (n - i); j++) {
+                row = row + (j + 1) + " ";
+            }
+            System.out.println(row);
+        }
+    }
+}
