c0df6ba3dcfc4d82bbfd9670b6d1ea16090e1bfb
HarshalSolao <harshalsolao@gmail.com>
2026-01-02 16:07:38 +0530

    Move all zeros to the end of array

---
+++ src/main/java/com/harshal/dsa/arrays/MoveZeroes.java
@@ -0,0 +1,26 @@
+package com.harshal.dsa.arrays;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/move-zeroes/
+public class MoveZeroes {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[x] = nums[i];
+                x++;
+            }
+        }
+        while (x < nums.length) {
+            nums[x] = 0;
+            x++;
+        }
+    }
+}
